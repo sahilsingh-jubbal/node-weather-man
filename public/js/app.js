723833c6d97dd460c272870a467c4b4b6d1d220a
unknown <you@example.com>
2021-03-13 01:36:55 +0530

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('form');
+const paraLoading = document.getElementById('loading');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputText = document.getElementById('name').value;
+    const para = document.getElementById('para');
+
+    paraLoading.innerText = 'Loading....';
+    fetch(`/weather?address=${inputText}`)
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.error) {
+                paraLoading.innerText = '';
+                para.innerText = data.error;
+            }
+            else {
+                paraLoading.innerText = '';
+                para.innerText = `showing weather for ${data.location}, temperature is ${data.temperature}°C and having ${data.Forecasting} weather.`
+            }
+        })
+        .catch((e) => {
+            console.log(e);
+            console.log('OOPs somthing is wrong! Error was occured');
+        })
+
+})
+
+
